c83fa4cea15a082c39f3f72008bc45f0407202b1
recurze <rahulv.9942@gmail.com>
2023-11-07 04:15:42 +0800

    Add get_plot.py

---
+++ get_plot.py
@@ -0,0 +1,37 @@
+from imdb import Cinemagoer
+import csv
+import multiprocessing
+import pandas as pd
+import pathlib
+
+
+cinemagoer = Cinemagoer()
+
+
+def get_plot(tid):
+    try:
+        plot = cinemagoer.get_movie(tid[2:], info=["plot"]).get("plot", " ")[0].strip().strip('"')
+    except Exception:
+        plot = ""
+
+    return tid, plot
+
+
+if __name__ == "__main__":
+    plots = {}
+    num_votes_threshold = 10000  # rougly 14500 titles, 1.5h
+
+    filepath = pathlib.PurePath("data", "imdb", "title.ratings.tsv")
+    df = pd.read_csv(filepath, delimiter='\t')
+    tids = df[df.numVotes > num_votes_threshold].tconst.tolist()
+
+    with open('plots.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["tconst", "plot"])
+
+        for i in range(0, len(tids), 400):
+            with multiprocessing.Pool() as pool:
+                plots = pool.map(get_plot, tids[i: i + 400])
+
+            writer.writerows(plots)
+            print(i + 400)
